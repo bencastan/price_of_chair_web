c07aee49dd33796451ac98d7017c5b0389303a5d
Ben Castan <ben@castanfamily.com>
2018-05-09 20:58:11 +1000

    Setting up and such Project 5 Lesson 8 Completed to end of Lesson 86 Loading item prices with requests and BeautifulSoup

--- src/models/items/items.py
+++ src/models/items/items.py
@@ -1,10 +1,28 @@
+import re
+import requests
+from bs4 import BeautifulSoup
 
 
 class Item(object):
-    def __init__(self, name, price, url):
+    def __init__(self, name, url, store):
         self.name = name
-        self.price = price
         self.url = url
+        self.store = store
+        tag_name = store.tag_name()
+        query = store.query()
+        self.price = self.load_price(tag_name, query)
 
     def __repr__(self):
         return "<Item {} with URL {}>".format(self.name, self.url)
+
+    def load_price(self, tag_name, query):
+        # Amazon: <span id="priceblock_ourprice" class="a-size-medium a-color-price">$4,032.00</span>
+        request = requests.get(self.url)
+        content = request.content
+        soup = BeautifulSoup(content, "html_parser")
+        element = soup.find(tag_name, query)
+        string_price = element.text.strip()
+        pattern = re.compile("(\d+.\d+)")  # $4,032.00
+        match = pattern.search(string_price)
+
+        return match.group()
--- src/models/items/views.py
+++ src/models/items/views.py
@@ -8,9 +8,3 @@ def item_page(name):
     pass
 
 
-@item_blueprint.route('/load')
-def load_item():
-    '''
-    Loads an items data using their store and return a JSON file
-    :return:
-    '''
